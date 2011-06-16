5b6ca5c52b9a3e52dff7a2428be80bea3e239330
fercolchero (BaSTA ) <colchero@imada.sdu.dk>
2011-06-16 08:23:58 +0000

    Made BayesSurvPlots.R compatible with the outputs of BayesSurv.R and BayesSurvParall.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayessurv@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/BayesSurvPlots.R
+++ pkg/R/BayesSurvPlots.R
@@ -2,35 +2,73 @@ BayesSurvPlots <-
 function(outBS){
 	require(RColorBrewer)
 	if(.Platform$OS.type=="unix") devtype=quartz else devtype=windows
-for(i in dev.list()) dev.off()
-# PLOTS:
-# Trace plots for parameters:
-if(!is.element("2", dev.list())) devtype(width=8, height=6)
-dev.set(2)
-par(mfrow=c(sum(outBS$modm[outBS$idm,]), ncol(outBS$Z)), mar=c(3,2,2,1))
-for(i in 1:ncol(outBS$theta)) plot(outBS$theta[,i], type='l', xlab="Iteration", ylab="", main=colnames(outBS$theta)[i])
+	for(i in dev.list()) dev.off()
 
-if(!is.element("3", dev.list())) devtype(width=8, height=6)
-dev.set(3)
-par(mfrow=c(ceiling(ncol(outBS$pi)/2+1),2), mar=c(3,2,2,1))
-for(i in 1:ncol(outBS$pi)) plot(outBS$pi[,i], type='l', xlab="", ylab="", main=paste("pi", i))
-for(i in 1:ncol(outBS$post)) plot(outBS$post[,i], type='l', xlab="Iteration", ylab=expression(p(theta|X), p(X[0]|X[1],theta,pi))[i])
+	# PLOTS:
+	# Trace plots for parameters:
+	parname     = c("theta","pi")
+	np          = length(parname)
+	nz          = ncol(outBS$Z)
+	nth         = sum(outBS$modm[outBS$idm,])
+	npi         = ceiling((ncol(outBS$pi)+3)/2)
+	postname    = expression(paste(p,"(",theta," | ",X,")"), paste(p,"(",X[0]," | ",X[1],",",theta,",",pi,",",")"), paste(p,"(",X[0],",",theta,",",pi,"| ... ",")"))
+	for(p in 1:np){
+		x        = outBS[[parname[p]]]
+		if(!is.element(as.character(p+1), dev.list())) devtype(width=8, height=6)
+		dev.set(as.character(p+1))
+		par(mfrow=c(c(nth, npi)[p], nz), mar=c(3,3,2,1))
+		for(i in 1:ncol(x)){
+			if(ncol(x)==1 ) cname = "pi" else cname = colnames(x)
+			if(length(dim(x))<3){
+				plot(x[,i], type='l', xlab="Iteration", ylab="", main=cname[i], frame.plot=FALSE)
+			} else {
+				yl    = range(x[,i,])
+				plot(c(1,nrow(x)), yl, col=NA, xlab="Iteration", ylab="", main=cname[i], frame.plot=FALSE)
+				for(j in 1:dim(x)[3]) lines(x[,i,j], type='l', col=brewer.pal(12, "Paired")[j])
+			}
+		}
+		if(p==np){			
+			x        = outBS$post
+			for(i in 1:ncol(x)){
+				if(length(dim(x))<3){
+					plot(x[,i], type='l', xlab="Iteration", main=postname[i])
+				} else {
+					yl       = range(x[,i,])
+					plot(c(1,nrow(x)), yl, col=NA, xlab="Iteration", main=postname[i],  frame.plot=FALSE)
+					for(j in 1:dim(x)[3]) lines(x[,i,j], type='l', col=brewer.pal(12, "Paired")[j])
+				} 
+			}
+		}		
+	}
+	
 
 
-# PLOTS:
-# Resulting survival and mortality:
-if(!is.element("4", dev.list())) devtype(width=6, height=6)
-dev.set(4)
-xv         = seq(0, max(outBS$xqsum), 0.1)
-Bord       = brewer.pal(12, "Paired")[round(seq(1,12, length=ncol(outBS$Z)))]
-Cols       = adjustcolor(Bord, alpha.f=0.5)
-par(mfrow=c(2,1))
-plot(range(xv), c(0,1), col=NA, xlab="Age", ylab=expression(S(x)), main="Survival probability")
-for(i in 1:ncol(outBS$Z)) polygon(c(xv, rev(xv)), c(outBS$Sxsum[[i]][2,], rev(outBS$Sxsum[[i]][3,])), col=Cols[i], border=Bord[i])
-legend('topright', colnames(outBS$Z), pch=15, pt.cex=2, col=Cols, bty='n')
+	# PLOTS:
+	# Resulting survival and mortality:
+	if(!is.element("4", dev.list())) devtype(width=6, height=6)
+	dev.set(4)
+	xv         = seq(0, max(outBS$xqsum), 0.1)
+	Bord       = brewer.pal(12, "Paired")[round(seq(1,12, length=ncol(outBS$Z)))]
+	Cols       = adjustcolor(Bord, alpha.f=0.5)
+	mx         = outBS$mxsum
+	Sx         = outBS$Sxsum
+	ylmx       = c(0, ifelse(nz>1, max(unlist(mx)), max(mx)))
+	
+	par(mfrow=c(2,1))
+	plot(range(xv), c(0,1), col=NA, xlab="", ylab=expression(S(x)), main="Survival probability", frame.plot=FALSE)
+	if(nz>1){
+		for(i in 1:nz) polygon(c(xv, rev(xv)), c(Sx[[i]][2,], rev(Sx[[i]][3,])), col=Cols[i], border=Bord[i])
+		legend('topright', colnames(outBS$Z), pch=15, pt.cex=2, col=Cols, bty='n')
+	} else {
+		polygon(c(xv, rev(xv)), c(Sx[2,], rev(Sx[3,])), col=Cols[1], border=Bord[1])
+	}
 
-plot(range(xv), c(0,max(unlist(outBS$mxsum))), col=NA, xlab="Age", ylab=expression(mu(x)), main="Mortality rate")
-for(i in 1:ncol(outBS$Z)) polygon(c(xv, rev(xv)), c(outBS$mxsum[[i]][2,], rev(outBS$mxsum[[i]][3,])), col=Cols[i], border=Bord[i])
+	plot(range(xv), ylmx, col=NA, xlab="Age (x)", ylab=expression(mu(x)), main="Mortality rate", frame.plot=FALSE)
+	if(nz>1){
+		for(i in 1:nz) polygon(c(xv, rev(xv)), c(mx[[i]][2,], rev(mx[[i]][3,])), col=Cols[i], border=Bord[i])	
+	} else {
+		polygon(c(xv, rev(xv)), c(mx[2,], rev(mx[3,])), col=Cols[1], border=Bord[1])
+	}
 
 }
 
