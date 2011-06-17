82fab2650f8471978c2f24fd943c5fc1f4c9267f
jonesor (Dr ) <jonesor@gmail.com>
2011-06-17 15:18:41 +0000

    Updated function - tested and working
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayessurv@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/makecovm.R
+++ pkg/R/makecovm.R
@@ -1,32 +1,64 @@
 makecovm <-
 function(Data,covs) {
 
-#covs =c("Assig","rand") ; Data = newdata
+#covs =c("Sex") ; Data = newdata
 
-nd = Data[,which(names(Data)%in%covs)]
+nd = data.frame(Data[,which(names(Data)%in%covs)])
+if(ncol(nd)==1) names(nd)=covs
+
 Cl = as.vector(sapply(nd,class))
+
+temp = c(Cl == "factor"|Cl == "numeric")
+if(sum(temp)<length(Cl)) stop("Data types for the covariates should be 'factor' or 'numeric'")
+
+factorCovs  = which(Cl=="factor")
+numericCovs = which(Cl=="numeric")
+
+if(length(factorCovs) >  1)  FactCovs = as.factor(apply(nd[,factorCovs],1,paste,collapse="."))
+if(length(factorCovs) == 1) FactCovs = nd[,factorCovs]
+if(length(factorCovs) == 0) FactCovs = NULL
+
+if(length(numericCovs) != 0) NumCovs = nd[,which(Cl=="numeric")]
+if(length(numericCovs) == 0) NumCovs = NULL
+
+if(is.null(FactCovs)) {tempCovs = data.frame(NumCovs)} 
+if(is.null(NumCovs)) {tempCovs = data.frame(FactCovs)}
+if(!is.null(NumCovs)&!is.null(FactCovs)) {tempCovs = data.frame(FactCovs,NumCovs)}
 
 Z=NULL
 
-for (x in 1:length(covs)){
+for (x in 1:ncol(tempCovs)){
 
-if(Cl[x] == "numeric"){Z = cbind(Z,nd[,x])}
 
-if(Cl[x] == "factor"){
+if(class(tempCovs[,x]) == "numeric"){
+    Z = cbind(Z,tempCovs[,x])
+    }
 
-    vL = length(nd[,x])
-    nLevs = length(levels(nd[,x]))
+if(class(tempCovs[,x]) == "factor"){
+
+    vL = length(tempCovs[,x])
+    nLevs = length(levels(tempCovs[,x]))
     a = (vL * nLevs) - nLevs
-    sv = seq(0, a, nLevs) + as.numeric(nd[,x])
+    sv = seq(0, a, nLevs) + as.numeric(tempCovs[,x])
     MV = rep(0, vL * nLevs)
     MV[sv] = 1
     M = matrix(MV, ncol = nLevs, nrow = vL, byrow = TRUE)
-    colnames(M) = levels(nd[,x])
+    colnames(M) = levels(tempCovs[,x])
     
     Z = cbind(Z,M)
 
 }
+
+
 }
+
+#Assign column names to the numeric variables (if there are any)
+if(length(numericCovs)!=0){
+if(is.null(colnames(Z))) {colnames(Z) = rep("",length(numericCovs))}
+colnames(Z)[colnames(Z)==""] = names(nd)[Cl=="numeric"]
+}
+
+#head(Z)
 
 return(Z)
 
